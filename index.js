d4a92379e3b012eb8bfda02a584d41b3dab1f41d
Rubab Arshad <160515174+rubabarshad@users.noreply.github.com>
2024-07-15 21:48:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt([{
+        name: 'userInput',
+        type: 'number',
+        message: "Please enter the amount of second",
+        validate: (input) => {
+            if (input === undefined || isNaN(input)) {
+                return "please enter valid number";
+            }
+            else if (input > 60) {
+                return "seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }]);
+let input = res.userInput;
+function startTime(val) {
+    let initTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(initTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDifference = differenceInSeconds(intervalTime, currentTime);
+        if (timeDifference <= 0) {
+            console.log("Timer has expired.");
+            process.exit();
+        }
+        const min = Math.floor((timeDifference % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDifference % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
